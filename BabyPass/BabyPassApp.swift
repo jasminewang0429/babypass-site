df94a95c0f759705b44ae95d49ba7588a0f7de4b
Jasmine <jasminewang0429@gmail.com>
2026-04-15 22:08:51 -0400

    Initial Commit

---
+++ BabyPass/BabyPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyPassApp.swift
+//  BabyPass
+//
+//  Created by Jasmine Wang on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct BabyPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabyPass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BabyPass
+//
+//  Created by Jasmine Wang on 4/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
